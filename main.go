048705c8c7343faa906f4c07ef82684c002d910d
Nikita <ragel56879@gmail.com>
2026-01-11 18:25:42 +0300

    first commit

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"sync"
+	"time"
 )
 
 const (
@@ -12,29 +15,103 @@ const (
 // generateRandomElements generates random elements.
 func generateRandomElements(size int) []int {
 	// ваш код здесь
+	list := make([]int, size)
+	if size == 0 {
+		return nil
+	}
+
+	for i := 0; i < size; i++ {
+		list[i] = rand.Int()
+	}
+	return list
 }
 
 // maximum returns the maximum number of elements.
 func maximum(data []int) int {
 	// ваш код здесь
+	if len(data) == 0 {
+		return 0
+	}
+	maxNum := data[0]
+	for _, v := range data {
+		if v > maxNum {
+			maxNum = v
+		}
+	}
+	return maxNum
 }
 
 // maxChunks returns the maximum number of elements in a chunks.
 func maxChunks(data []int) int {
 	// ваш код здесь
+	if len(data) == 0 {
+		return 0
+	}
+	if len(data) < CHUNKS {
+		maxi := data[0]
+		for _, v := range data {
+			if v > maxi {
+				maxi = v
+			}
+		}
+		return maxi
+	}
+
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+
+	size := len(data) / CHUNKS
+	var eightMax []int
+
+	for i := 0; i < CHUNKS; i++ {
+		wg.Add(1)
+		st := i * size
+		end := st + size
+		if end > len(data) || i == CHUNKS-1 {
+			end = len(data)
+		}
+
+		go func(st, end int) {
+			defer wg.Done()
+			maxNum := data[st]
+			for _, v := range data[st:end] {
+				if v > maxNum {
+					maxNum = v
+				}
+			}
+			mu.Lock()
+			eightMax = append(eightMax, maxNum)
+			mu.Unlock()
+		}(st, end)
+	}
+	wg.Wait()
+
+	maxi := eightMax[0]
+	for _, v := range eightMax {
+		if v > maxi {
+			maxi = v
+		}
+	}
+	return maxi
 }
 
 func main() {
-	fmt.Printf("Генерируем %d целых чисел", SIZE)
+	fmt.Printf("Генерируем %d целых чисел\n", SIZE)
 	// ваш код здесь
+	list := generateRandomElements(SIZE)
 
 	fmt.Println("Ищем максимальное значение в один поток")
+
 	// ваш код здесь
+	start := time.Now()
+	maxNumber := maximum(list)
+	elapsed := time.Now().Sub(start).Microseconds()
+	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", maxNumber, elapsed)
 
-	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
-
-	fmt.Printf("Ищем максимальное значение в %d потоков", CHUNKS)
+	fmt.Printf("Ищем максимальное значение в %d потоков\n", CHUNKS)
 	// ваш код здесь
-
-	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
+	start = time.Now()
+	maxNumber = maxChunks(list)
+	elapsed = time.Now().Sub(start).Microseconds()
+	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", maxNumber, elapsed)
 }
--- main_test.go
+++ main_test.go
@@ -1,3 +1,71 @@
 package main
 
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
 // Пишите тесты в этом файле
+func TestGenerateRandomElements(t *testing.T) {
+	// 0 элементов
+	require.Nil(t, nil, generateRandomElements(0), "Ожидается: %v\nПолучено: %v", nil, generateRandomElements(0))
+}
+
+func TestMaximum(t *testing.T) {
+	// пустой слайс
+	var data []int
+	expected := 0
+	require.Equal(t, expected, maximum(data), "Ожидается: %d\nПолучено: %d", expected, maximum(data))
+
+	// слайс с 1 элементом
+	data = []int{10}
+	expected = 10
+	require.Equal(t, expected, maximum(data), "Ожидается: %d\nПолучено: %d", expected, maximum(data))
+
+	// слайс с отрицательными числами
+	data = []int{-1, -5, -3, -2, -10}
+	expected = -1
+	require.Equal(t, expected, maximum(data), "Ожидается: %d\nПолучено: %d", expected, maximum(data))
+
+	// слайс с повторяющими значениями
+	data = []int{3, 3, 3, 3, 3, 3}
+	expected = 3
+	require.Equal(t, expected, maximum(data), "Ожидается: %d\nПолучено: %d", expected, maximum(data))
+}
+
+func TestMaxChunks(t *testing.T) {
+	// пустой слайс
+	var data []int
+	expected := 0
+	require.Equal(t, expected, maxChunks(data), "Ожидается: %d\nПолучено: %d", expected, maxChunks(data))
+
+	// слайс с 1 элементом
+	data = []int{5}
+	expected = 5
+	require.Equal(t, expected, maxChunks(data), "Ожидается: %d\nПолучено: %d", expected, maxChunks(data))
+
+	// слайс с кол-вом элементов <CHUNKS
+	data = []int{1, 2, 3, 7, 5, 2}
+	expected = 7
+	require.Equal(t, expected, maxChunks(data), "Ожидается: %d\nПолучено: %d", expected, maxChunks(data))
+
+	// слайс с кол-вом элементов ==CHUNKS
+	data = []int{1, 2, 3, 40, 5, 6, 7, 10}
+	expected = 40
+	require.Equal(t, expected, maxChunks(data), "Ожидается: %d\nПолучено: %d", expected, maxChunks(data))
+
+	// слайс с кол-вом элементов >CHUNKS % CHUNKS != 0
+	data = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	expected = 10
+	require.Equal(t, expected, maxChunks(data), "Ожидается: %d\nПолучено: %d", expected, maxChunks(data))
+
+	// слайс с отрицательными числами
+	data = []int{-1, -5, -3, -2, -10}
+	expected = -1
+	require.Equal(t, expected, maxChunks(data), "Ожидается: %d\nПолучено: %d", expected, maxChunks(data))
+
+	// слайс с повторяющими значениями
+	data = []int{3, 3, 3, 3, 3, 3}
+	expected = 3
+	require.Equal(t, expected, maxChunks(data), "Ожидается: %d\nПолучено: %d", expected, maxChunks(data))
+}
